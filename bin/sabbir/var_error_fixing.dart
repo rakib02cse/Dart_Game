02091f7edc6951d2289e82cd4b5d05a6d5f2f4b2
sabbir <sabbircvgc11@gmail.com>
2025-08-27 15:56:07 +0600

    use comment heare

---
+++ bin/sabbir/var_error_fixing.dart
@@ -0,0 +1,11 @@
+//const name ='Ray';
+//name += 'wenderlich';
+
+//here book use const ,basically const variable are cant assign a value.
+//thats why we use varour code
+
+void main() {
+  var name = 'sabbir';
+  name += 'mozumder';
+  print(name);
+}
