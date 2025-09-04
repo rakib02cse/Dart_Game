02be36e9daf84055bd440cbd068f25c11c1a9c67
sabbir <sabbircvgc11@gmail.com>
2025-09-05 04:02:40 +0600

    final and const concept

--- bin/sabbir/chapter_6/runtime_compiletime.dart
+++ bin/sabbir/chapter_6/runtime_compiletime.dart
@@ -4,4 +4,13 @@ void main() {
   final time = DateTime.now(); //final datatype can 1 time change in runtime
 
   print(time);
+
+  //another example
+  const sabbir = [1, 2, 3];
+  final rakib = [1, 2, 3];
+
+  //sabbir.add(4);
+  rakib.add(4);
+  print(sabbir);
+  print(rakib);
 }
