785aa1f0df9344576a7f9e3e100374284a32a410
sabbir <sabbircvgc11@gmail.com>
2025-09-05 03:52:10 +0600

    final and const concept clear

---
+++ bin/sabbir/chapter_6/runtime_compiletime.dart
@@ -0,0 +1,7 @@
+void main() {
+  //const time = DateTime.now(); // here, when i write const my code is errore , when i am write final my code is work .
+
+  final time = DateTime.now(); //final datatype can 1 time change in runtime
+
+  print(time);
+}
