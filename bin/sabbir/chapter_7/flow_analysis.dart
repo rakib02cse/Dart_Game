1dbb415620dbddae883019806d33cfa476a1f1eb
sabbir <sabbircvgc11@gmail.com>
2025-09-15 02:02:32 +0600

    flow analysis

---
+++ bin/sabbir/chapter_7/flow_analysis.dart
@@ -0,0 +1,13 @@
+void main() {
+  int anInteger = -50;
+
+  bool isPositive(int? anInteger) {
+    if (anInteger == null) {
+      return false;
+    }
+    return !anInteger.isNegative;
+  }
+
+  print(anInteger);
+  print(isPositive(anInteger));
+}
