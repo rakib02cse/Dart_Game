c8c8090512549015e127e8a8966bc6c7643089ba
sabbir <sabbircvgc11@gmail.com>
2025-09-15 03:07:03 +0600

    null aware access operator

---
+++ bin/sabbir/chapter_7/null_aware_assignment_operator.dart
@@ -0,0 +1,11 @@
+void main() {
+  String? fontSize;
+
+  fontSize = fontSize ??= 'error'; // Null-aware assignment operator ??=
+
+  print(fontSize);
+
+  int? age = -20;
+  print(age?.isNegative); //Null-aware access operator ?.
+  print(age?.toDouble()); //Null-aware access operator ?.
+}
