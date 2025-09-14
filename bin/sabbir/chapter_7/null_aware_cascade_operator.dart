4353d23c68278a7b6a7d14e93a4a62af727c25c5
sabbir <sabbircvgc11@gmail.com>
2025-09-15 04:09:52 +0600

    null aware casecade oparator

---
+++ bin/sabbir/chapter_7/null_aware_cascade_operator.dart
@@ -0,0 +1,16 @@
+class User {
+  String? name;
+  int? id;
+}
+
+User? user = User()
+  ?..name = 'sabbir'
+  ..id = 20;
+
+void main() {
+  String? lengthString = user?.name?.length.toString();
+  double? idLength = user?.id?.toString().length.toDouble();
+
+  print(lengthString);
+  print(user?.id);
+}
