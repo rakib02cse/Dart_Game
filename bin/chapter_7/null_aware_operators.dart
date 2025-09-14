9604ade60736f1d4adf11c0a8edd17cbc52a19c5
Rakib Hossain <rakib62615@gmail.com>
2025-09-14 20:20:14 +0600

    null-aware cascade operator (?..)

--- bin/chapter_7/null_aware_operators.dart
+++ bin/chapter_7/null_aware_operators.dart
@@ -3,7 +3,7 @@
 // Null_aware access operator (?.)
 // null_aware method Invocation operator (?.)
 // null assertion operator (!)
-// null-aware cascad operator (?..)
+// null-aware cascade operator (?..)
 // null-aware indes operator (?[])
 // null-aware spred operator (..?)
 
@@ -12,6 +12,27 @@ void main(){
   String? message = 'Rakib';
   final text = message ?? 'Error'; // null- aware assignment operator
   print(text);
+  
   int age=20;
-  print ( age?. toDouble());// null-aware method invocation operator
+  print ( age?.toDouble());// null-aware method invocation operator
+
+  String myNullableString = 'Rakib';
+
+  String nonNullableString = myNullableString!;
+  print(nonNullableString);
+
+
+  User user = User()
+   ?..name = 'Nasim'
+   ..id = 42;
+   print('name: ${user.name}');
+   print('id: ${user.id}');
 }
+
+class User {
+
+  String? name ;
+  int? id;
+  
+}
+
