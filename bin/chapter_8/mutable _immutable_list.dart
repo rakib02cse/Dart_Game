70f6e82fe99cdd11567a1b4918a5feb27789966b
Rakib Hossain <rakib62615@gmail.com>
2025-09-18 20:11:58 +0600

    spread operator

--- bin/chapter_8/mutable _immutable_list.dart
+++ bin/chapter_8/mutable _immutable_list.dart
@@ -1,13 +1,17 @@
 void main (){
   var desserts = ['cookies', 'cupcakes', 'donuts','pie']; // if we use Final 
-  // desserts = [];  // not allowed
-  // desserts = [];  // not allowed
-  // desserts = ['cookies', 'cupcakes', 'donuts', 'pie']; // not allowed
+  desserts = [];  // not allowed
+  desserts = [];  // not allowed
+  desserts = ['cookies', 'cupcakes', 'donuts', 'pie']; // not allowed
+
+  
 
 
   desserts.remove('cookies');
   desserts.add('ice cream');
 
-  print(desserts);
+  desserts.forEach(print); // Higher-order function 
+
 
 }
+  
