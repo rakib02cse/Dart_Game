5615817a7463146954f7e774608de342d62ff47e
Rakib Hossain <rakib62615@gmail.com>
2025-09-20 15:57:47 +0600

    collection for

---
+++ bin/chapter_8/collection_for.dart
@@ -0,0 +1,10 @@
+void main() {
+  const deserts = ['gobi', 'sahara', 'arctic', 'good'];
+
+  var bigDeserts = [
+    'ARABIN',
+    for (var desert in deserts) desert.toUpperCase(),
+  ];
+
+  print(bigDeserts);
+}
