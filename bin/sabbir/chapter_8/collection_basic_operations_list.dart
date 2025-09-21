8125503c0bf02e558bca05ad8d8cea177c8643ab
sabbir <sabbircvgc11@gmail.com>
2025-09-21 08:40:44 +0600

    update c_8 collection basic

--- bin/sabbir/chapter_8/collection_basic_operations_list.dart
+++ bin/sabbir/chapter_8/collection_basic_operations_list.dart
@@ -6,10 +6,10 @@
 
 void main() {
   var desserts = ['cookie', 'cha', 'banana', 'pie'];
-  final index = desserts.indexOf('pie');
-  final value = desserts[index];
+  //final index = desserts.indexOf('pie');
+  //final value = desserts[index];
 
-  final secondElement = desserts[3];
+  final secondElement = desserts[2];
 
   final modifiableList = [DateTime.now()];
   final unmodifiableList = List.unmodifiable(modifiableList);
@@ -18,5 +18,5 @@ void main() {
   print(modifiableList);
 
   print(secondElement);
-  print(value);
+  //print(value);
 }
