829e53f8fda8c05765b164c0e19313e1f18c9d83
sabbir <sabbircvgc11@gmail.com>
2025-09-23 08:27:32 +0600

    intersection & union

---
+++ bin/sabbir/chapter_8/intersection_union.dart
@@ -0,0 +1,10 @@
+void main() {
+  final setA = {8, 2, 3, 1, 4};
+  final setB = {1, 6, 5, 4};
+
+  //final intersection = setA.intersection(setB);
+  final Union = setA.union(setB);
+
+  //print(intersection);
+  print(Union);
+}
