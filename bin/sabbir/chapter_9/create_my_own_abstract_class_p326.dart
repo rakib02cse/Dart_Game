d93e0d947aeb85b82f03adb0bb5e09da3440a1bb
sabbir <sabbircvgc11@gmail.com>
2025-09-29 15:49:53 +0600

    update createmy own abstract class p326

--- bin/sabbir/chapter_9/create_my_own_abstract_class_p326.dart
+++ bin/sabbir/chapter_9/create_my_own_abstract_class_p326.dart
@@ -1,6 +1,13 @@
 void main() {
   var dog = Dog(); // subclass object create
   print(dog); //dog.toString() implicit vabe call hbe
+
+  final platypus = Platypus();
+  print(platypus.isAlive);
+  platypus.eat();
+  platypus.move();
+  platypus.layEggs();
+  print(platypus);
 }
 
 abstract class Animal {
@@ -24,5 +31,21 @@ class Dog extends Animal {
   @override
   void move() {
     print("Dog is running");
+  }
+}
+
+class Platypus extends Animal {
+  @override
+  void eat() {
+    print("Platypus is eating insects");
+  }
+
+  @override
+  void move() {
+    print("Platypus is swimming");
+  }
+
+  void layEggs() {
+    print("Platypus is laying eggs");
   }
 }
